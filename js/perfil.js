745bf70fe2e4951d4f7f627f64bc2206906173ef
Eduardo Nascimento Couto Luiz <24123950@cfp127aluno.local>
2025-05-08 08:51:35 -0300

    tela perfil

---
+++ js/perfil.js
@@ -0,0 +1,27 @@
+'use stict'
+
+// Cria uma função assíncrona
+async function profile (){
+
+    const username = document.getElementById('username')
+    const perfilImg = document.getElementById('perfilImg')
+
+    // Nessa var, é necessário integrar com o id do user logado
+    let idUsuario = 1
+
+    // EndPoint que lista usuários pelo Id (perfil)
+    const dataUser = await fetch(`https://back-spider.vercel.app/user/pesquisarUser/${idUsuario}`) 
+
+    // Converte a resposta da API em JSON, armazenando os dados do usuário na variável user
+    const user = await dataUser.json()
+
+    console.log(user);
+
+    username.textContent = user.nome
+    perfilImg.src = user.imagemPerfil
+    
+
+    
+}
+
+profile()
